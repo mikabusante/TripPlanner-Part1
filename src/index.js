c733bf9ae77dd143ac034a41415b20434a73c5bb
Mika Busante <mika@mikabusante.com>
2019-11-18 12:11:22 -0500

    Add marker, did not get picture to work completely right

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-console.log("hiiiiii");
-console.log("hellloooo");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWlrYWJ1c2FudGUiLCJhIjoiY2szNG9janU5MTdrOTNocHBkc243Y2NkbyJ9.t4BlWGiD0pdV-PimxORHew";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = document.createElement("div");
+marker.style.width = "32px";
+marker.style.height = "32px";
+marker.style.backgroundImage =
+  "url(https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg)";
+new mapboxgl.Marker(marker).setLngLat([-74.009151, 40.705086]).addTo(map);
