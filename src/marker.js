1b05fb59b978f1ac06e9904f285f8219908052ff
Mika Busante <mika@mikabusante.com>
2019-11-18 13:04:31 -0500

    Add css

--- src/marker.js
+++ src/marker.js
@@ -1,14 +1,11 @@
-
 const mapboxgl = require("mapbox-gl");
 
-
-function makeMarker(url) {
-const marker = document.createElement("div");
-marker.style.width = "32px";
-marker.style.height = "32px";
-marker.style.backgroundImage = `url(${url})`;
-return new mapboxgl.Marker(marker).setLngLat([-74.009151, 40.705086]);
-
+function makeMarker(url, coordinate) {
+  const marker = document.createElement("div");
+  marker.style.width = "32px";
+  marker.style.height = "32px";
+  marker.style.backgroundImage = `url(${url})`;
+  return new mapboxgl.Marker(marker).setLngLat([-74.009151, 40.705086]);
 }
 
 module.exports = makeMarker;
